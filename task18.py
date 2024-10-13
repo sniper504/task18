8750a33d98ea08956f99463e059c0be897ee4848
sniper504 <abracadabra52@tutamail.com>
2024-10-13 23:14:32 +0300

    task 18 ready

--- task18.py
+++ task18.py
@@ -1,10 +1,13 @@
-input_data=open("input.txt","r")#data=data.split()n=int(data[0])
+input_data=open("input.txt","r")#def factorial(n):   if(n==0):   return 1     else: return n*factorial(n-1)
 data =input_data.read()
-output_data=open("output.txt","a")
-def factorial(n):
-        if(n==0):
-                return 1
-        else: return n*factorial(n-1)
+data=data.split()
+n=int(data[0])#
+for f in range(1,n+1):
+        if(f==0):
+                n=1
+else:
+        n=f*(f-1)
+        output_data=open("output.txt","w")
         output_data.write(str(n))
 input_data.close()
 output_data.close()
