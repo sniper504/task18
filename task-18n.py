115cd4d60f32b363a628d2fad23263e59bc79e4d
sniper504 <abracadabra52@tutamail.com>
2024-10-14 12:12:03 +0300

    Update task-18n.py

--- task-18n.py
+++ task-18n.py
@@ -1,4 +1,4 @@
-input_data=open("input.txt","r")#def factorial(n):   if(n==0):   return 1     else: return n*factorial(n-1)
+input_data=open("input.txt","r")
 data =input_data.read()
 data=data.split()
 n=int(data[0])#
